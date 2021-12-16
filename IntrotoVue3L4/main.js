f01802889bb18aad6cc58e9f1581dba4ebaa2baf
Chaya-Yellow <69381752+Chaya-Yellow@users.noreply.github.com>
2021-12-16 16:06:50 +0800

    完成L4任务并改进界面设计

--- IntrotoVue3L4/main.js
+++ IntrotoVue3L4/main.js
@@ -5,7 +5,28 @@ const app11 = Vue.createApp({
             product:'Socks',
             image:'./assets/images/socks_blue.jpg',
             url:'https://www.vuemastery.com/courses/intro-to-vue-3/intro-to-vue3',
-            inStock:true
+            // inStock:true,
+            inventory:9,
+            onSale:true,
+            delails:['50% cotton','30% wool','20% polyester'],
+            variants:[{id:2234,color:'green',size:'36',image:"./assets/images/socks_green.jpg"},
+                      {id:2235,color:'blue',size:'37',image:"./assets/images/socks_blue.jpg"}],
+            cart:0
+            // sizes:[{id:2234,yard:'36'},
+            //           {id:2235,yard:'37'}]
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        updateImage(variantImage){
+            this.image=variantImage
+        },
+        decToCart(){
+            if(this.cart>0){
+                this.cart -=1
+            }
         }
     }
 })
